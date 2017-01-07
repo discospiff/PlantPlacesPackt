7c2358cc63a6b15fece78403b7e006bc3d5b10b7
jonesbr <brandan.jones@uc.edu>
2017-01-07 20:42:38 +1100

    Initial commit of PlantPlacesPackt project. Video 1.4

---
+++ app/src/main/java/jonesbl/packt/com/plantplacespackt/SearchPlantsActivity.java
@@ -0,0 +1,13 @@
+package jonesbl.packt.com.plantplacespackt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchPlantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_plants);
+    }
+}
