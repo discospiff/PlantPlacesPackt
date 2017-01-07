7a093556dddc22e6518f4a5ef4b808b989a347a0
jonesbr <brandan.jones@uc.edu>
2017-01-07 23:42:16 +1100

    3.3 JUnit Annotations: @Test, @Before, @BeforeClass, @After, @AfterClass

--- app/src/test/java/jonesbl/packt/com/plantplacespackt/TestPlantDAO.java
+++ app/src/test/java/jonesbl/packt/com/plantplacespackt/TestPlantDAO.java
@@ -1,6 +1,9 @@
 package jonesbl.packt.com.plantplacespackt;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
@@ -20,9 +23,15 @@ public class TestPlantDAO {
     // define a variable for the DAO we are testing.
     IPlantDAO plantDAO;
 
+    @BeforeClass
+    public static void setupAllTests() {
+        System.out.println("BeforeClass: running init for ALL tests.");
+    }
+
     @Before
     public void setup() {
         plantDAO = new PlantDAOStub();
+        System.out.println("Before: running init before EACH tests.");
     }
 
     @Test
@@ -40,6 +49,45 @@ public class TestPlantDAO {
         }
         // did we find a redbud?
         assertTrue(redbudFound);
+        System.out.println("TEST: Running redbud test.");
+    }
+
+    @Test
+    public void testPlantDAO_searchForOakShouldReturnAtLeastOneWhiteOak() {
+
+        // assume we do not have a match.
+        boolean whiteOakFound = false;
+
+        List<PlantDTO> plants = plantDAO.fetchPlants("Oak");
+
+        for (PlantDTO plant : plants) {
+            if (plant.getGenus().equalsIgnoreCase("Quercus") && plant.getSpecies().contains("alba")) {
+                whiteOakFound = true;
+            }
+        }
+        // did we find a redbud?
+        assertTrue(whiteOakFound);
+        System.out.println("TEST: Running white oak test.");
+    }
+
+    public void testPlantDAO_searchForEShouldReturnAtLeastTwoResults() {
+        List<PlantDTO> plants = plantDAO.fetchPlants("e");
+        int size = plants.size();
+        boolean atLeastTwo = size > 2;
+        assertTrue(atLeastTwo);
+        System.out.println("TEST: this should not run, because it is not annotated.");
+        // assertThat(size, is(greaterThan(2)));
+    }
+
+    @After
+    public void teardown() {
+        System.out.println("After: Tearing down EACH test.");
+
+    }
+
+    @AfterClass
+    public static void teardownAllTests() {
+        System.out.println("AfterClass: tearing down after ALL tests.");
     }
 
 }
